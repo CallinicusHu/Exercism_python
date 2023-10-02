59dcf2128d13ca7da4f996bec8ed8420ed11ec28
Callinicus <szemetdiszno@gmail.com>
2023-10-02 17:22:16 +0200

    even bobber

--- bob/bob.py
+++ bob/bob.py
@@ -21,9 +21,11 @@ def response(hey_bob):
 
     return reply[x]
     """
-   
-    if len(hey_bob.strip()) < 1: return "Fine. Be that way!"
-    elif (hey_bob.strip()[-1] == "?") and hey_bob.isupper(): return "Calm down, I know what I'm doing!"
-    elif hey_bob.strip()[-1] == "?":return "Sure."
+
+    hey_bob = hey_bob.strip()
+    
+    if len(hey_bob) < 1: return "Fine. Be that way!"
+    elif (hey_bob[-1] == "?") and hey_bob.isupper(): return "Calm down, I know what I'm doing!"
+    elif hey_bob[-1] == "?": return "Sure."
     elif hey_bob.isupper(): return "Whoa, chill out!"
     else: return "Whatever."
