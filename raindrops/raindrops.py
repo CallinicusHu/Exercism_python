2ebfdc0ce9dee41aa97242516e1f394cfc44505c
Callinicus <szemetdiszno@gmail.com>
2023-10-27 01:17:40 +0200

    raindrops

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,10 +1,20 @@
 def convert(number):
 
-    drop = ["Pling", "Plang", "Plong"]
+    ppp = ""
+    a = True
+    b = True
+    c = True
 
-    if 1 == 2:
-        1
-        
-    else:
-        
-        return str(number)
+    if number % 3 == 0:
+        ppp = ppp + "Pling"
+        a = False
+    if number % 5 == 0:
+        ppp = ppp + "Plang"
+        b = False
+    if number % 7 == 0:
+        ppp = ppp + "Plong"
+        c = False
+    if a and b and c:
+        ppp = str(number)
+
+    return ppp
