a7d39c3bb872241099f41c259014231c8feb62dc
Callinicus <szemetdiszno@gmail.com>
2023-11-11 15:57:48 +0100

    flatten array fail

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -2,10 +2,15 @@ def flatten(iterable):
 
     final = []
 
-    for leng in range(len(iterable)):
-        if iterable[leng] or iterable[leng] == 0:
-            final.append(iterable[leng])
+    while iterable: # amíg van tartlama az iterablenek
+        for length in range(len(iterable)): #menjen végig az iterable hosszán
+            if iterable[length] or iterable[length] == 0: #ha az iterable aktuális eleme létezik vagy 0
+                final.append(iterable[length].pop) # adja hozzá a finalhöz az iterable aktuális elemét amit ezzel töröl is az iterableből
+    #végtelen ciklus indexerror typeerror minden baja van
 
     print(f"\n{final}")
 
     return final
+
+flatty = [0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2]
+#flatten(flatty)
