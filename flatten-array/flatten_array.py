0082b9d7ca8527972355ed242d090e0b41493a7a
Callinicus <szemetdiszno@gmail.com>
2023-11-11 18:21:37 +0100

    flatten array cheated

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -5,9 +5,9 @@ def flatten(iterable):
 
     for numbers in range(len(cheat_str)):
         if cheat_str[numbers].isdigit() or cheat_str[numbers] == "-":
-            add_to_int = add_to_int + cheat_str[numbers]
+            add_to_int += cheat_str[numbers]
         else:
-            add_to_int = add_to_int + " "
+            add_to_int += " "
 
     for hole in add_to_int.split():
         final.append(eval(hole))
