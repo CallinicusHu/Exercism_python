7442c979a091f3b226af4102cf6a2e1994a91152
Callinicus <szemetdiszno@gmail.com>
2023-11-11 18:14:12 +0100

    flatten array cheated

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,45 +1,15 @@
 def flatten(iterable):
-
-    """
+    cheat_str = str(iterable)
     final = []
+    add_to_int = ""
 
-    while iterable: # amíg van tartlama az iterablenek
-        for length in range(len(iterable)): #menjen végig az iterable hosszán
-            if iterable[length] or iterable[length] == 0: #ha az iterable aktuális eleme létezik vagy 0
-                final.append(iterable.pop(length)) # adja hozzá a finalhöz az iterable aktuális elemét amit ezzel töröl is az iterableből
-    #végtelen ciklus indexerror typeerror minden baja van
-
-    print(f"\n{final}")
-    """
-
-
-    final = []
-    tempor = iterable.copy()
-    print(iterable)
-    #print(tempor)
-
-    for next_in_iterable in range(len(iterable)):
-        if type(iterable[next_in_iterable]) != list:
-            if iterable[next_in_iterable] or iterable[next_in_iterable] == 0:
-                final.append(iterable[next_in_iterable])
+    for numbers in range(len(cheat_str)):
+        if cheat_str[numbers].isdigit() or cheat_str[numbers] == "-":
+            add_to_int = add_to_int + cheat_str[numbers]
         else:
-            final.append(deep(iterable[next_in_iterable]))
+            add_to_int = add_to_int + " "
 
-    print(final)
+    for hole in add_to_int.split():
+        final.append(eval(hole))
 
     return final
-
-def deep(zoom):
-
-    for next in range(len(zoom)):
-        if type(zoom[next]) != list:
-            if zoom[next] or zoom[next] == 0:
-                return zoom[next] # ha a next nem lista tegye a final végére a 26on
-        else:
-            deep(next) # ha next lista menjen újra amíg nem az
-
-    print("zoom", zoom)
-    return
-
-flatty = [0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2]
-flatten(flatty)
