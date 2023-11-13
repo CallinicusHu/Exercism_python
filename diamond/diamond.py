d9d201e8ce9391e4ac65b1ea26cd3fe7240622ea
Callinicus <szemetdiszno@gmail.com>
2023-11-13 12:13:17 +0100

    diamond unfinished

---
+++ diamond/diamond.py
@@ -0,0 +1,22 @@
+def rows(letter):
+
+    return [" A ", "B B", " A "]
+
+
+print(rows("a"))
+
+"""
+What the exercise describes:
+    A    
+   B B   
+  C   C  
+ D     D 
+E       E
+ D     D 
+  C   C  
+   B B   
+    A    
+    
+What the exercise actually expects:
+["  A  ", " B B ", "C   C", " B B ", "  A  "]
+"""
