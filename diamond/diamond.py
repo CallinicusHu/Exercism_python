9399138162c61bd03f0e7746bbdefd0e0938534c
Callinicus <szemetdiszno@gmail.com>
2023-11-18 18:22:43 +0100

    diamond again

--- diamond/diamond.py
+++ diamond/diamond.py
@@ -1,18 +1,56 @@
 def rows(letter):
+    print("\n")
+    abc = "abcdefghijklmnopqrstuvwxyz".upper()
+    where = abc.index(letter)  # the index position of the given letter in alphabet, B = 1
+
+    diamond = f"{' ' * where}A{' ' * where}\n" # first A line with spaces
+
+    for build in range(0, where):
+        letter = abc[build + 1]
+        position = (where - build -1)
+        diamond += f"{' ' * position}{letter}{' ' * ((build*2) + 1)}{letter}{' ' * position}\n"
+
+    for build in range((where -2), -1, -1):
+        letter = abc[build + 1]
+        position = (where - build - 1)
+        diamond += f"{' ' * position}{letter}{' ' * ((build*2) + 1)}{letter}{' ' * position}\n"
+
+    if where > 0:
+        diamond += f"{' ' * where}A{' ' * where}\n" # last A line with spaces
+
+    print(diamond)
+
+    return diamond.splitlines()
+
+    """
+    print("\n")
     abc = "abcdefghijklmnopqrstuvwxyz".upper()
     where = abc.index(letter)  # the index position of the given letter in alphabet, B = 1
     size = max(where * 2 + 1, 1)  # this is how many columns and lines the diamond should have
 
     diamond = f"{' ' * where}A{' ' * where}\n" # first A line with spaces
 
-    for build in range(size - 2):
-        diamond += f"{' ' * size}\n" # body of the diamond with spaces
+
+    #for build in range(size - 2):
+    #    diamond += f"{' ' * size}\n" # body of the diamond with spaces
+    for build in range(0, where):
+        letter = abc[build + 1]
+        position = (where - build -1)
+        diamond += f"{' ' * position}{letter}{' ' * ((build*2) + 1)}{letter}{' ' * position}\n"
+
+    for build in range((where -2), -1, -1):
+        letter = abc[build + 1]
+        position = (where - build - 1)
+        diamond += f"{' ' * position}{letter}{' ' * ((build*2) + 1)}{letter}{' ' * position}\n"
+
 
     if where > 0:
         diamond += f"{' ' * where}A{' ' * where}\n" # last A line with spaces
-
+    """
+    """
     size += 1 #lines got 1 index longer because of \n
 
+    
     for count, build in enumerate(range(size, size * (where + 1), size)):
         position = build + where - count - 1
         diamond = f"{diamond[:position]}{abc[count + 1]}{diamond[position + 1:]}"
@@ -24,20 +62,22 @@ def rows(letter):
         diamond = f"{diamond[:position]}{abc[count + 1]}{diamond[position + 1:]}"
         position = build - where - count - 3 # I have no idea why is this -3 but it is
         diamond = f"{diamond[:position]}{abc[count + 1]}{diamond[position + 1:]}"
-
+    """
+    """
     print(f"\n{diamond}\n") # At this point we print out the diamond shape I think the exercise described.
 
-    diamond = diamond.replace("\n", "") # actual task does not require linebreaks
-    size -= 1
+    #diamond = diamond.replace("\n", "") # actual task does not require linebreaks
+    #size -= 1
 
-    diamond_list = []
+    diamond_list = diamond.splitlines()
 
-    for rebuild in range(0, len(diamond), size):
-        diamond_list.append(diamond[rebuild:rebuild+size])
+    #for rebuild in range(0, len(diamond), size):
+    #    diamond_list.append(diamond[rebuild:rebuild+size])
 
     print(diamond_list) # At this point we print out the diamond shape I think the exercise expects.
 
     return diamond_list
+    """
 
 """
 What the exercise describes:
