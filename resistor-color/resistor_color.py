0a829762196443bd4c1e387a4bf4e29d7429da97
Callinicus <szemetdiszno@gmail.com>
2023-11-21 11:26:06 +0100

    resistor color should this be a list, dict or tuple? (all work)

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,4 +1,4 @@
-code = {
+"""code = {
         "black": 0,
         "brown": 1,
         "red": 2,
@@ -9,10 +9,16 @@ code = {
         "violet": 7,
         "grey": 8,
         "white": 9
-    }
+    }"""
+
+code = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+#works both with the dict and the list, list only as long the order is not changed (tuple would be safer?)
 
 def color_code(color):
-    return code.get(color)
+    #return code.get(color)
+    return code.index(color)
 
 def colors():
-    return list(code.keys())
+    #return list(code.keys())
+    return code
