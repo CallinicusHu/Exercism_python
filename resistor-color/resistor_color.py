dec329b5f8849da4ca07bd19ab0451cb5cecbff3
Callinicus <szemetdiszno@gmail.com>
2023-11-21 11:19:48 +0100

    resistor color done, sublist unequal fail

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,18 @@
+code = {
+        "black": 0,
+        "brown": 1,
+        "red": 2,
+        "orange": 3,
+        "yellow": 4,
+        "green": 5,
+        "blue": 6,
+        "violet": 7,
+        "grey": 8,
+        "white": 9
+    }
+
+def color_code(color):
+    return code.get(color)
+
+def colors():
+    return list(code.keys())
