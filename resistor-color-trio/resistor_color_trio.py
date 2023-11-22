ef05ed4a2bc4e9910f177aa704eca7bf4f6ad2fd
Callinicus <szemetdiszno@gmail.com>
2023-11-22 03:00:52 +0100

    resistor color trio #["red", "black", "red"] still fails

--- resistor-color-trio/resistor_color_trio.py
+++ resistor-color-trio/resistor_color_trio.py
@@ -1,34 +1,31 @@
 code = ("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
-def label(colors): #I actually thing this is flawed but passes the tests
-    if colors[2] == "black":
-        ohms =  value(colors) + " ohms"
-    if colors[2] == "brown":
-        ohms = value(colors) + "0 ohms"
-    if colors[2] == "red":
-        ohms = value(colors) + " kiloohms" #shouldn't this be "00 ohms"? Maybe because the test say ["red", "black", "red"] it passes, but it feels wrong.
-    if colors[2] == "orange":
-        ohms = value(colors) + " kiloohms"
-    if colors[2] == "yellow":
-        ohms = value(colors) + "0 kiloohms"
-    if colors[2] == "green":
-        ohms = value(colors) + "00 kilohms"
-    if colors[2] == "blue":
-        ohms = value(colors) + " megaohms"
-    if colors[2] == "violet":
-        ohms = value(colors) + "0 megaohms"
-    if colors[2] == "grey":
-        ohms = value(colors) + "00 megaohms"
-    if colors[2] == "white":
-        ohms = value(colors) + " gigaohms"
+def label(colors): #["red", "black", "red"] still fails
 
-    if ohms[0] == "0":
-        ohms = ohms[1:]
 
-    if ohms[1] == "0" and ohms[0] != "0":
-        ohms = ohms[0:1]+ohms[2:]
+    ohms = ((10 * code.index(colors[0])) + (code.index(colors[1]))) * pow(10, code.index(colors[2]))
+    ohms_str = str(ohms)
 
-    return ohms
+    if ohms == 0:
+        return "0 ohms"
 
-def value(colors):
-    return str(code.index(colors[0]))+(str(code.index(colors[1])))
-
+    if ohms % 1_000_000_000 == 0: #"white":
+        return str(ohms_str[:2]) + " gigaohms"
+    if ohms % 100_000_000 == 0: #"grey":
+        return str(ohms_str[:2]) + "00 megaohms"
+    if ohms % 10_000_000 == 0: #"violet":
+        return str(ohms_str[:2]) + "0 megaohms"
+    if ohms % 1_000_000 == 0: #"blue":
+        return str(ohms_str[:2]) + " megaohms"
+    if ohms % 100_000 == 0: #"green":
+        return str(ohms_str[:2]) + "00 kilohms"
+    if ohms % 10_000 == 0: #"yellow":
+        return str(ohms_str[:2]) + "0 kiloohms"
+    if ohms % 1000 == 0: #"orange":
+        return str(ohms_str[:2]) + " kiloohms"
+    if ohms % 100 == 0: #"red":
+        return str(ohms_str[:2]) + "00 ohms"
+    if ohms % 10 == 0: #"brown":
+        return str(ohms_str[:2]) + "0 ohms"
+    if ohms % 1 == 0: #"black":
+        return str(ohms_str[:2]) + " ohms"
+    
