2e6a56c6337a2434e8a3cfbf6525a03eab270110
Callinicus <szemetdiszno@gmail.com>
2023-11-23 09:56:38 +0100

    resistor color trio good

--- resistor-color-trio/resistor_color_trio.py
+++ resistor-color-trio/resistor_color_trio.py
@@ -1,31 +1,30 @@
 code = ("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
-def label(colors): #["red", "black", "red"] still fails
 
 
-    ohms = ((10 * code.index(colors[0])) + (code.index(colors[1]))) * pow(10, code.index(colors[2]))
-    ohms_str = str(ohms)
+def label(colors):
 
-    if ohms == 0:
+    ohms_str = str(((10 * code.index(colors[0])) + (code.index(colors[1]))) * pow(10, code.index(colors[2])))
+
+    if ohms_str == "0":
         return "0 ohms"
 
-    if ohms % 1_000_000_000 == 0: #"white":
-        return str(ohms_str[:2]) + " gigaohms"
-    if ohms % 100_000_000 == 0: #"grey":
-        return str(ohms_str[:2]) + "00 megaohms"
-    if ohms % 10_000_000 == 0: #"violet":
-        return str(ohms_str[:2]) + "0 megaohms"
-    if ohms % 1_000_000 == 0: #"blue":
-        return str(ohms_str[:2]) + " megaohms"
-    if ohms % 100_000 == 0: #"green":
-        return str(ohms_str[:2]) + "00 kilohms"
-    if ohms % 10_000 == 0: #"yellow":
-        return str(ohms_str[:2]) + "0 kiloohms"
-    if ohms % 1000 == 0: #"orange":
-        return str(ohms_str[:2]) + " kiloohms"
-    if ohms % 100 == 0: #"red":
-        return str(ohms_str[:2]) + "00 ohms"
-    if ohms % 10 == 0: #"brown":
-        return str(ohms_str[:2]) + "0 ohms"
-    if ohms % 1 == 0: #"black":
-        return str(ohms_str[:2]) + " ohms"
-    
+    if ohms_str.count("0") == 9:
+        return ohms_str.replace("0", "") + " gigaohms"
+    if ohms_str.count("0") == 8:
+        return ohms_str.replace("0", "") + "00 megaohms"
+    if ohms_str.count("0") == 7:
+        return ohms_str.replace("0", "") + "0 megaohms"
+    if ohms_str.count("0") == 6:
+        return ohms_str.replace("0", "") + " megaohms"
+    if ohms_str.count("0") == 5:
+        return ohms_str.replace("0", "") + "00 kilohms"
+    if ohms_str.count("0") == 4:
+        return ohms_str.replace("0", "") + "0 kiloohms"
+    if ohms_str.count("0") == 3:
+        return ohms_str.replace("0", "") + " kiloohms"
+    if ohms_str.count("0") == 2:
+        return ohms_str.replace("0", "") + "00 ohms"
+    if ohms_str.count("0") == 1:
+        return ohms_str.replace("0", "") + "0 ohms"
+    if ohms_str.count("0") == 0:
+        return ohms_str.replace("0", "") + " ohms"
