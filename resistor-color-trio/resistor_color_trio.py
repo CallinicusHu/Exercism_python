c049066ed56e1a0794c6c4e2876f58d8f2c108ac
Callinicus <szemetdiszno@gmail.com>
2023-11-23 21:48:12 +0100

    resistor color trio armin

--- resistor-color-trio/resistor_color_trio.py
+++ resistor-color-trio/resistor_color_trio.py
@@ -8,23 +8,10 @@ def label(colors):
     if ohms_str == "0":
         return "0 ohms"
 
-    if ohms_str.count("0") == 9:
-        return ohms_str.replace("0", "") + " gigaohms"
-    if ohms_str.count("0") == 8:
-        return ohms_str.replace("0", "") + "00 megaohms"
-    if ohms_str.count("0") == 7:
-        return ohms_str.replace("0", "") + "0 megaohms"
-    if ohms_str.count("0") == 6:
-        return ohms_str.replace("0", "") + " megaohms"
-    if ohms_str.count("0") == 5:
-        return ohms_str.replace("0", "") + "00 kilohms"
-    if ohms_str.count("0") == 4:
-        return ohms_str.replace("0", "") + "0 kiloohms"
-    if ohms_str.count("0") == 3:
-        return ohms_str.replace("0", "") + " kiloohms"
-    if ohms_str.count("0") == 2:
-        return ohms_str.replace("0", "") + "00 ohms"
-    if ohms_str.count("0") == 1:
-        return ohms_str.replace("0", "") + "0 ohms"
-    if ohms_str.count("0") == 0:
-        return ohms_str.replace("0", "") + " ohms"
+    suffix_code = ohms_str.count("0") // 3
+    zeros = ohms_str.count("0") % 3
+    suffix = {0: " ohms", 1: " kiloohms", 2: " megaohms", 3: " gigaohms"}
+    ohms_str = ohms_str.replace("0", "")
+
+    return f"{ohms_str}{'0' * zeros}{suffix.get(suffix_code)}"
+
