75bc0e6421a3649678cd4e29a9772b262a832df4
Callinicus <szemetdiszno@gmail.com>
2023-11-25 16:06:53 +0100

    prime factors (I failed the math)

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,2 +1,7 @@
 def translate(text):
-    pass
+
+    if isvowel(text[0]) or text.startswith("xr") or text.startswith("yt"):
+        return text + "ay"
+
+def isvowel(letter):
+    return letter == "a" or "e" or "i" or "o" or "u"
---
+++ prime-factors/prime_factors.py
@@ -0,0 +1,33 @@
+def factors(value):
+    print("\n", value, "\n")
+    primes = find_primes(value)
+    primes.append(value)
+    prime_factors = []
+    temporary = value
+    count = 0
+
+    while temporary > 1:
+        if temporary % primes[count] == 0:
+            temporary //= primes[count]
+            prime_factors.append(primes[count])
+            print(temporary)
+        else:
+            count += 1
+
+    print("prime factors", prime_factors, "\n---")
+    return prime_factors
+
+
+def find_primes(value):
+    primes = []
+    for numbers in range(2, (value // 2) + 1):
+        print(numbers)
+        prime = True
+        for number in range(2, numbers):
+            if numbers % number == 0:
+                prime = False
+        if prime:
+            primes.append(numbers)
+
+    print("primes", primes)
+    return primes
