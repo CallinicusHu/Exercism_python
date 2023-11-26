4759ffc62c5ddcc8f083ef86eabb91a96fe1ea0b
Callinicus <szemetdiszno@gmail.com>
2023-11-26 03:30:30 +0100

    matching_brackets (kinda nice but fail)

---
+++ matching-brackets/matching_brackets.py
@@ -0,0 +1,20 @@
+import string
+
+brackets = list("[](){}")
+
+def is_paired(input_string):
+
+    """ My nice but halfbaked and failed idea
+    cntr = []
+
+    for items in range(len(brackets)):
+        cntr.append(input_string.count(brackets[items]))
+
+    if (cntr[0] == cntr[1]) and (cntr[2] == cntr[3]) and (cntr[4] == cntr[5]):
+        print("right amounts")
+        return True
+    """
+
+
+    return False
+
