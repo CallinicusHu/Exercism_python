c112ddc13e8992f3a13f8a1d0e36b70ded4b2f77
Callinicus <szemetdiszno@gmail.com>
2023-12-11 22:57:00 +0100

    resistor color expert worse? better? same?

--- resistor-color-expert/resistor_color_expert.py
+++ resistor-color-expert/resistor_color_expert.py
@@ -6,29 +6,24 @@ suffix = {0: "ohms", 1: "kiloohms", 2: "megaohms", 3: "gigaohms"}
 def resistor_label(colors):
     if len(colors) == 1:
         return f"{str(code.index(colors[0]))} {suffix[0]}"
-    if len(colors) == 4:
-        return label4(colors)
-    if len(colors) == 5:
-        return label5(colors)
+    elif len(colors) == 4:
+        ohms_str = label4(colors)
+    else:
+        ohms_str = label5(colors)
+
+    if ohms_str == "0":
+        return f"{ohms_str} {suffix[0]}" #aka "0 ohms"
+
+    return f"{ohms_begins(ohms_str)}{ohms_cont(ohms_str)} {suffix[suffixcode(ohms_str)]} {tolerance.get(colors[-1])}"
 
 def label4(colors):
-    ohms_str = str(((10 * code.index(colors[0])) + (code.index(colors[1]))) * pow(10, code.index(colors[2])))
-
-    if ohms_str == "0":
-        return "0 ohms"
-
-    return f"{ohms_begins(ohms_str)}{ohms_cont(ohms_str)} {suffix[suffixcode(ohms_str)]} {tolerance.get(colors[3])}"
-
+    return str(
+        ((10 * code.index(colors[0])) + (code.index(colors[1]))) * pow(10, code.index(colors[2])))
 
 def label5(colors):
-    ohms_str = str(
+    return str(
         (((100 * code.index(colors[0]))) + (10 * (code.index(colors[1]))) + (code.index(colors[2]))) *
         pow(10, code.index(colors[3])))
-
-    if ohms_str == "0":
-        return "0 ohms"
-
-    return f"{ohms_begins(ohms_str)}{ohms_cont(ohms_str)} {suffix[suffixcode(ohms_str)]} {tolerance.get(colors[4])}"
 
 
 def suffixcode(ohms_str):
@@ -41,7 +36,7 @@ def ohms_begins(ohms_str):
         return f"{ohms_str[:len(ohms_str) % 3]}"
 
 def ohms_cont(ohms_str):
-    if ohms_str.count("0") % 3 == 0 or len(ohms_str) < 4:
+    if ohms_str.count('0') % 3 == 0 or len(ohms_str) < 4:
         return ""
     else:
-        return f".{ohms_str[len(ohms_str) % 3:len(ohms_str) - ohms_str.count("0")]}"
+        return f".{ohms_str[len(ohms_str) % 3:len(ohms_str) - ohms_str.count('0')]}"
