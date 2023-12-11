c5f0943ea16c466f663eee35313d00cd7ad613ad
Callinicus <szemetdiszno@gmail.com>
2023-12-11 22:41:59 +0100

    resistor color expert good?

--- resistor-color-expert/resistor_color_expert.py
+++ resistor-color-expert/resistor_color_expert.py
@@ -1,18 +1,15 @@
 code = ("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
 tolerance = {"grey": "±0.05%", "violet": "±0.1%", "blue": "±0.25%",
              "green": "±0.5%", "brown": "±1%", "red": "±2%", "gold": "±5%", "silver": "±5%"}
-suffix = {0: " ohms", 1: " kiloohms", 2: " megaohms", 3: " gigaohms"}
-print()
-
+suffix = {0: "ohms", 1: "kiloohms", 2: "megaohms", 3: "gigaohms"}
 
 def resistor_label(colors):
     if len(colors) == 1:
-        return str(code.index(colors[0])) + " ohms"
+        return f"{str(code.index(colors[0]))} {suffix[0]}"
     if len(colors) == 4:
         return label4(colors)
     if len(colors) == 5:
         return label5(colors)
-
 
 def label4(colors):
     ohms_str = str(((10 * code.index(colors[0])) + (code.index(colors[1]))) * pow(10, code.index(colors[2])))
@@ -20,14 +17,7 @@ def label4(colors):
     if ohms_str == "0":
         return "0 ohms"
 
-    suffix_code = suffixcode(ohms_str)
-    zeros = zerocount(ohms_str)
-    ohms_str = removezeros(ohms_str)
-
-    if len(ohms_str) + zeros == 4:
-        return f"{ohms_str[:len(ohms_str) - 1]}.{ohms_str[len(ohms_str) - 1:]}{suffix.get(suffix_code + 1)} {tolerance.get(colors[3])}"
-    else:
-        return f"{ohms_str}{'0' * zeros}{suffix.get(suffix_code)} {tolerance.get(colors[3])}"
+    return f"{ohms_begins(ohms_str)}{ohms_cont(ohms_str)} {suffix[suffixcode(ohms_str)]} {tolerance.get(colors[3])}"
 
 
 def label5(colors):
@@ -38,27 +28,20 @@ def label5(colors):
     if ohms_str == "0":
         return "0 ohms"
 
-    print(ohms_str)
-
-    suffix_code = suffixcode(ohms_str)
-    zeros = zerocount(ohms_str)
-    ohms_str = removezeros(ohms_str)
-
-    print(ohms_str)
-
-    if len(ohms_str) + zeros > 3:
-        return f"{ohms_str[:len(ohms_str) - 2]}.{ohms_str[len(ohms_str) - 2:]}{suffix.get(suffix_code + 1)} {tolerance.get(colors[4])}"
-    else:
-        return f"{ohms_str}{'0' * zeros}{suffix.get(suffix_code)} {tolerance.get(colors[4])}"
+    return f"{ohms_begins(ohms_str)}{ohms_cont(ohms_str)} {suffix[suffixcode(ohms_str)]} {tolerance.get(colors[4])}"
 
 
 def suffixcode(ohms_str):
-    return ohms_str.count("0") // 3
+    return (len(ohms_str) - 1) // 3
 
+def ohms_begins(ohms_str):
+    if len(ohms_str) % 3 == 0:
+        return ohms_str[:3]
+    elif len(ohms_str) % 3 != 0:
+        return f"{ohms_str[:len(ohms_str) % 3]}"
 
-def zerocount(ohms_str):
-    return ohms_str.count("0") % 3
-
-
-def removezeros(ohms_str):
-    return ohms_str.replace("0", "")
+def ohms_cont(ohms_str):
+    if ohms_str.count("0") % 3 == 0 or len(ohms_str) < 4:
+        return ""
+    else:
+        return f".{ohms_str[len(ohms_str) % 3:len(ohms_str) - ohms_str.count("0")]}"
