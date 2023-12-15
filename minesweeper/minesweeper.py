d2d5e2fafc25c33eb107d6b34d46b4f49570304e
Callinicus <szemetdiszno@gmail.com>
2023-12-15 09:23:48 +0100

    minesweeper why it counts wrong :-(

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,6 +1,4 @@
-
 def annotate(minefield):
-
     check = []
     for line in minefield:
         for char in line:
@@ -10,37 +8,23 @@ def annotate(minefield):
     if check and sum(check) != check[0] * len(check):
         raise ValueError("The board is invalid with current input.")
 
-    mines = 0
+    iterate = [[-1, -1], [-1, +0], [-1, +1],
+               [+0, -1], [+0, +1],
+               [+1, -1], [+1, +0], [+1, +1]]
 
     for i, line in enumerate(minefield):
         for c, pos in enumerate(line):
+
             if pos == " ":
-
-                if i > 0 and c > 0 and minefield[i - 1][c - 1] == "*":
-                    mines += 1
-                if i > 0 and minefield[i - 1][c] == "*":
-                    mines += 1
-                if i > 0 and c < len(line) - 1 and minefield[i - 1][c + 1] == "*":
-                    mines += 1
-
-                if c > 0 and minefield[i][c - 1] == "*":
-                    mines += 1
-                if c < len(line) - 1 and minefield[i][c + 1] == "*":
-                    mines += 1
-
-                if i < len(minefield) - 1 and c > 0 and minefield[i + 1][c - 1] == "*":
-                    mines += 1
-                if i < len(minefield) - 1 and minefield[i + 1][c] == "*":
-                    mines += 1
-                if i < len(minefield) - 1 and c < len(line) - 1 and minefield[i + 1][c + 1] == "*":
-                    mines += 1
+                mines = 0
+                for lin, col in iterate:
+                    try:
+                        if minefield[i + lin][c + col] == "*":
+                            mines += 1
+                    except:
+                        continue
 
                 if mines > 0:
-
-                    minefield[i] = f"{minefield[i][:c]}{mines}{minefield[i][c+1:]}"
-
-                mines = 0
-
-
+                    minefield[i] = f"{minefield[i][:c]}{mines}{minefield[i][c + 1:]}"
 
     return minefield
