f30ef002115dcd8c43745c295c7e30c391c537bc
Callinicus <szemetdiszno@gmail.com>
2023-12-18 13:50:21 +0100

    minesweeper is THAT if fine?

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,5 +1,6 @@
 import itertools
 
+
 def annotate(minefield):
     check = []
     for line in minefield:
@@ -10,21 +11,19 @@ def annotate(minefield):
     if check and sum(check) != check[0] * len(check):
         raise ValueError("The board is invalid with current input.")
 
-    mines = 0
-
     iterate = list(itertools.product([-1, 0, 1], [-1, 0, 1]))
     del iterate[4]
 
     for i, lines in enumerate(minefield):
         for c, col in enumerate(lines):
+            mines = 0
             if minefield[i][c] == " ":
                 for y, x in iterate:
-                    if ((len(minefield) - 1) >= (i + y) >= 0) and ((len(lines) - 1) >= (c + x) >= 0) and minefield[i + y][c + x] == "*":
-
+                    if ((len(minefield) > (i + y) >= 0) and (len(lines) > (c + x) >= 0) and
+                            minefield[i + y][c + x] == "*"):
                         mines += 1
 
             if mines > 0:
                 minefield[i] = f"{minefield[i][:c]}{mines}{minefield[i][c + 1:]}"
-            mines = 0
 
     return minefield
