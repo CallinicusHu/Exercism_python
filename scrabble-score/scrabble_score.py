3583c0e177c8a87c7bb63cfd56bd5a7acbcace33
Callinicus <szemetdiszno@gmail.com>
2023-12-18 15:57:57 +0100

    scrabble score (slightly better?)

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,11 +1,7 @@
-
-onepoint = dict.fromkeys(("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"), 1)
-twopoint = dict.fromkeys(("D", "G"), 2)
-threepoint = dict.fromkeys(("B", "C", "M", "P"), 3)
-fourpoints = dict.fromkeys(("F", "H", "V", "W", "Y"), 4)
-morepoints = {"K": 5, "J": 8, "X": 8, "Q": 10, "Z": 10}
-
-scoring = {**onepoint, **twopoint, **threepoint, **fourpoints, **morepoints}
+scoring = {**(dict.fromkeys(("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"), 1)),
+           **(dict.fromkeys(("B", "C", "M", "P"), 3)),
+           **(dict.fromkeys(("F", "H", "V", "W", "Y"), 4)),
+           **{"K": 5, "J": 8, "X": 8, "Q": 10, "Z": 10, "D": 2, "G": 2}}
 
 def score(word):
     return sum([scoring.get(letter.upper()) for letter in word])
