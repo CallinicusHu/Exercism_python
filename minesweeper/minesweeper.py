61d0c577f2f23180f880d6b56410c13d65f7a61c
Callinicus <szemetdiszno@gmail.com>
2023-12-18 13:33:58 +0100

    minesweeper is THAT if fine?

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,3 +1,5 @@
+import itertools
+
 def annotate(minefield):
     check = []
     for line in minefield:
@@ -8,23 +10,21 @@ def annotate(minefield):
     if check and sum(check) != check[0] * len(check):
         raise ValueError("The board is invalid with current input.")
 
-    iterate = [[-1, -1], [-1, +0], [-1, +1],
-               [+0, -1], [+0, +1],
-               [+1, -1], [+1, +0], [+1, +1]]
+    mines = 0
 
-    for i, line in enumerate(minefield):
-        for c, pos in enumerate(line):
+    iterate = list(itertools.product([-1, 0, 1], [-1, 0, 1]))
+    del iterate[4]
 
-            if pos == " ":
-                mines = 0
-                for lin, col in iterate:
-                    try:
-                        if minefield[i + lin][c + col] == "*":
-                            mines += 1
-                    except:
-                        continue
+    for i, lines in enumerate(minefield):
+        for c, col in enumerate(lines):
+            if minefield[i][c] == " ":
+                for y, x in iterate:
+                    if ((len(minefield) - 1) >= (i + y) >= 0) and ((len(lines) - 1) >= (c + x) >= 0) and minefield[i + y][c + x] == "*":
 
-                if mines > 0:
-                    minefield[i] = f"{minefield[i][:c]}{mines}{minefield[i][c + 1:]}"
+                        mines += 1
+
+            if mines > 0:
+                minefield[i] = f"{minefield[i][:c]}{mines}{minefield[i][c + 1:]}"
+            mines = 0
 
     return minefield
--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,27 +1,7 @@
 def recite(start_verse, end_verse):
 
-    x = [
-            "On the twelfth day of Christmas my true love gave to me: "
-            "twelve Drummers Drumming, "
-            "eleven Pipers Piping, "
-            "ten Lords-a-Leaping, "
-            "nine Ladies Dancing, "
-            "eight Maids-a-Milking, "
-            "seven Swans-a-Swimming, "
-            "six Geese-a-Laying, "
-            "five Gold Rings, "
-            "four Calling Birds, "
-            "three French Hens, "
-            "two Turtle Doves, "
-            "and a Partridge in a Pear Tree."
-        ]
+    day = ["first", "second", "third", "fourth", "fifth", "sixth"]
+    gifts = []
 
-    y = ("corgi panda"
-         "macs")
-    print(y)
 
-    print(type(x))
-    print(len(x))
-    print(x)
-    print(x[0])
-    return x
+    return [f"On the {day}  of Christmas my true love gave to me: {gifts}"]
