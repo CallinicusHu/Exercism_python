7c3f0ecfb3d4b3f5530908577a8100d0ab72bff2
Callinicus <szemetdiszno@gmail.com>
2024-02-20 14:36:57 +0100

    say: it works, but I have no idea how this solved the recursion which I do not know why had happened

--- say/say.py
+++ say/say.py
@@ -1,7 +1,10 @@
+import math
+
 num_up_to_9 = {0: "", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
 tenfolds = {1: "ten", 2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"}
 teens = {11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"}
 prefixes = {3: "thousand", 6: "million", 9: "billion"}
+
 
 def say(number):
     if number < 0:
@@ -49,33 +52,12 @@ def any_big(number):
     if number < 1_000:
         return up_till_999(number)
 
-    lennie = len(str(number))
-
-    if lennie < 6:
-        power = 3
-    elif lennie < 9:
-        power = 6
-    #elif lennie < 12:
-    #    power = 9
-    #else:
-    #    power = 12
-    else:
-        power = 9
-
-    # power = ??? #in one line / formula
+    power = (math.ceil(len(str(number)) / 3)) * 3 - 3
 
     mendokusai = f" {prefixes[power]} "
     if number % (10 ** power) == 0:
         mendokusai = f" {prefixes[power]}"
 
     return f"{any_big(number // (10 ** power))}{mendokusai}{any_big(number % (10 ** power))}"
-    # ???
-    """
-    >>> 987_654_321_123 % 10 ** 9 
-    654321123
-    >>> 987_654_321_123 // 10 ** 9
-    987
-
-    """
 
 
